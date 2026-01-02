1a9f12cd445566c44ba3ab77ca7aa2654028d82f
tmohr4 <mohrtru@outlook.com>
2026-01-02 10:08:49 -0500

    Added slider controls

---
+++ CreditModifier.cs
@@ -0,0 +1,93 @@
+﻿extern alias Legacy;
+global using HarmonyLib;
+global using RoR2;
+global using System;
+using BepInEx;
+using BepInEx.Configuration;
+using MiscFixes.Modules;
+using System.IO;
+using System.Reflection;
+using System.Security.Permissions;
+using UnityEngine.Networking;
+
+[assembly: AssemblyVersion(CreditModifier.version)]
+#pragma warning disable CS0618
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618
+
+
+[BepInPlugin(identifier, "MohrCredits", version)]
+public class CreditModifier : BaseUnityPlugin
+{
+	public const string identifier = "MohrCredits";
+	public const string version = "1.0.0";
+    private static Harmony instance = null;
+
+    static double m = 10;
+    static double b = 0;
+
+    public void Awake()
+	{
+        Load(Config);
+
+		Harmony.CreateAndPatchAll(typeof(CreditModifier));
+
+		Run.onRunStartGlobal += Begin;
+		Run.onRunDestroyGlobal += End;
+	}
+
+    public static void Load(ConfigFile cfg)
+    {
+
+        try { cfg.Reload(); }
+        catch (FileNotFoundException) { cfg.Clear(); }
+
+        string title = "Credit Modifier Controls";
+
+        CreditModifier.b = cfg.BindOptionSlider(title, "Flat", "Flat credit scalar", 0.5f).Value;
+        CreditModifier.m = cfg.BindOptionSlider(title, "Scalar", "Per-player credit scalar", 0.5f).Value;
+    }
+
+    public static void Begin(Run thisRun)
+    {
+        if (instance is null && NetworkServer.active)
+        {
+            instance = Harmony.CreateAndPatchAll(typeof(CreditModifier));
+
+            SceneDirector.onPrePopulateSceneServer += AdjustInteractableCredits;
+        }
+    }
+
+    public static void End(object _)
+    {
+        SceneDirector.onPrePopulateSceneServer -= AdjustInteractableCredits;
+
+        instance?.UnpatchSelf();
+        instance = null;
+    }
+
+    //	[HarmonyPatch(typeof(SceneDirector), nameof(SceneDirector.PopulateScene))]
+    //	[HarmonyPrefix]
+    private static void AdjustInteractableCredits(SceneDirector __instance)
+    {
+        SceneDef currentScene = SceneInfo.instance?.sceneDef;
+        string sceneName = currentScene?.baseSceneName;
+        bool hiddenRealms = sceneName == "arena" || sceneName == "voidstage" || currentScene?.sceneType == SceneType.Intermission;
+        if (hiddenRealms)
+        {
+            System.Console.WriteLine($"Prevent extra items in hidden realm \"{sceneName}\".");
+            return;
+        }
+
+        double playerCount = Run.instance.participatingPlayerCount;
+        double initialCredits = __instance.interactableCredit;
+        double baseCredits = initialCredits / (0.5 * playerCount + 0.5);
+        double extraCredits = baseCredits * (m * playerCount + b) - initialCredits;
+        extraCredits = Math.Round(extraCredits, MidpointRounding.AwayFromZero);
+
+        __instance.interactableCredit += (int)extraCredits;
+
+        System.Console.WriteLine($"adjusted initial {initialCredits} credits by {extraCredits} to {__instance.interactableCredit}.");
+        Run.instance.RecalculateDifficultyCoefficent();
+    }
+}
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MultitudesDifficulty.Properties {
+namespace MohrCredits.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MultitudesDifficulty.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MultitudesDifficulty.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MohrCredits.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -57,26 +57,6 @@ namespace MultitudesDifficulty.Properties {
             }
             set {
                 resourceCulture = value;
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] eclipse {
-            get {
-                object obj = ResourceManager.GetObject("eclipse", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] icon {
-            get {
-                object obj = ResourceManager.GetObject("icon", resourceCulture);
-                return ((byte[])(obj));
             }
         }
     }
